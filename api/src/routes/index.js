b8379752d7d73eb7d83120327e0a44f6bfa156b4
lcygler <leandrocygler@gmail.com>
2023-04-13 22:26:09 -0300

    update router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const getPokemons = require("../controllers/getPokemons");
+const getPokemonById = require("../controllers/getPokemonById");
+const getPokemonByName = require("../controllers/getPokemonByName");
+const createPokemon = require("../controllers/createPokemon");
+const getTypes = require("../controllers/getTypes");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/pokemons/:idPokemon", getPokemonById);
+router.get("/pokemons", getPokemonByName);
+router.get("/pokemons", getPokemons);
+router.get("/types", getTypes);
+router.post("/pokemons", createPokemon);
 
 module.exports = router;
