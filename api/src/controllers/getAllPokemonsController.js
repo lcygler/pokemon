ce40c74f43ed0f1fd5fa079806f4ef27ad2ef1ce
lcygler <leandrocygler@gmail.com>
2023-04-15 02:07:52 -0300

    update controllers

--- api/src/controllers/getAllPokemonsController.js
+++ api/src/controllers/getAllPokemonsController.js
@@ -2,44 +2,7 @@ const { Pokemon, Type } = require("../db.js");
 const { API_URL } = require("../utils/consts.js");
 const axios = require("axios");
 
-const getPokemon = async (pokemonName) => {
-  // Search pokemon in DB
-  const dbPokemon = await Pokemon.findOne({
-    where: {
-      name: pokemonName,
-    },
-    include: {
-      model: Type,
-    },
-  });
-
-  // Pokemon exists in DB
-  if (dbPokemon) {
-    return dbPokemon;
-  }
-
-  // GET pokemon from API
-  try {
-    const response = await axios.get(`${API_URL}/pokemon/${pokemonName}`);
-    const pokemon = {
-      id: response.data.id,
-      name: response.data.name,
-      hp: response.data.stats[0].base_stat,
-      attack: response.data.stats[1].base_stat,
-      defense: response.data.stats[2].base_stat,
-      speed: response.data.stats[5].base_stat,
-      height: response.data.height,
-      weight: response.data.weight,
-      image: response.data.sprites.other["official-artwork"]["front_default"],
-      types: response.data.types.map((element) => element.type.name),
-    };
-    return pokemon;
-  } catch (error) {
-    throw new Error("Pokemon not found");
-  }
-};
-
-const getAllPokemons = async () => {
+const getAllPokemonsController = async () => {
   // GET pokemons from API
   const response = await axios.get(`${API_URL}/pokemon?limit=3`); // total 1279
   const pokemons = response.data.results;
@@ -73,4 +36,4 @@ const getAllPokemons = async () => {
   return allPokemons;
 };
 
-module.exports = { getPokemon, getAllPokemons };
+module.exports = getAllPokemonsController;
---
+++ api/src/controllers/getPokemonController.js
@@ -0,0 +1,42 @@
+const { Pokemon, Type } = require("../db.js");
+const { API_URL } = require("../utils/consts.js");
+const axios = require("axios");
+
+const getPokemonController = async (pokemonName) => {
+  // Search pokemon in DB
+  const dbPokemon = await Pokemon.findOne({
+    where: {
+      name: pokemonName,
+    },
+    include: {
+      model: Type,
+    },
+  });
+
+  // Pokemon exists in DB
+  if (dbPokemon) {
+    return dbPokemon;
+  }
+
+  // GET pokemon from API
+  try {
+    const response = await axios.get(`${API_URL}/pokemon/${pokemonName}`);
+    const pokemon = {
+      id: response.data.id,
+      name: response.data.name,
+      hp: response.data.stats[0].base_stat,
+      attack: response.data.stats[1].base_stat,
+      defense: response.data.stats[2].base_stat,
+      speed: response.data.stats[5].base_stat,
+      height: response.data.height,
+      weight: response.data.weight,
+      image: response.data.sprites.other["official-artwork"]["front_default"],
+      types: response.data.types.map((element) => element.type.name),
+    };
+    return pokemon;
+  } catch (error) {
+    throw new Error("Pokemon not found");
+  }
+};
+
+module.exports = getPokemonController;
