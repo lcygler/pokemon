fbef71aa95e91480fd2da4112cf25e92a3bc375f
lcygler <leandrocygler@gmail.com>
2023-04-17 17:34:03 -0300

    add index files

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import Card from "./Card/Card";
+import Cards from "./Cards/Cards";
+import Navbar from "./Navbar/Navbar";
+
+export { Card, Cards, Navbar };
---
+++ client/src/views/index.js
@@ -0,0 +1,5 @@
+import Create from "./Create/Create";
+import Detail from "./Detail/Detail";
+import Home from "./Home/Home";
+
+export { Create, Detail, Home };
