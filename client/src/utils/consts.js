6529dddee35478bbf722f72dddba935e947c1849
lcygler <leandrocygler@gmail.com>
2023-04-21 18:01:10 -0300

    add regex

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,3 +1,10 @@
-module.exports = {
-  API_URL: "http://localhost:3001",
-};
+//* API
+export const API_URL = "http://localhost:3001";
+
+//* Regex
+export const lettersRegex = /^[a-zA-Z]+$/;
+
+export const imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
+
+export const urlRegex =
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
