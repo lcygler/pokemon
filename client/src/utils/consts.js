90fd4cde3a02626d97201048a3bd1e3439e3b5de
lcygler <leandrocygler@gmail.com>
2023-04-21 19:37:24 -0300

    add uuid regex

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -2,9 +2,11 @@
 export const API_URL = "http://localhost:3001";
 
 //* Regex
-export const lettersRegex = /^[a-zA-Z]+$/;
-
 export const imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
+
+export const lettersRegex = /^[a-zA-Z]+$/;
 
 export const urlRegex =
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
+export const uuidRegex = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[8|9|aA|bB][a-f0-9]{3}-[a-f0-9]{12}$/;
