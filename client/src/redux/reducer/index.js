129cf3929e8e986b745b5565f263f562603760b1
lcygler <leandrocygler@gmail.com>
2023-04-21 04:01:14 -0300

    update reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,13 +1,26 @@
-import { CLEAR_DETAIL, GET_ALL, GET_BY_ID, GET_BY_NAME, GET_TYPES } from "../actions";
+import { CLEAR_DETAIL, CREATE_POKEMON, GET_ALL, GET_BY_ID, GET_BY_NAME, GET_TYPES } from "../actions";
 
 const initialState = {
   allPokemons: [],
   selectedPokemon: {},
+  createdPokemon: {},
   types: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CLEAR_DETAIL:
+      return {
+        ...state,
+        selectedPokemon: {},
+      };
+
+    case CREATE_POKEMON:
+      return {
+        ...state,
+        createdPokemon: action.payload,
+      };
+
     case GET_ALL:
       return {
         ...state,
@@ -30,12 +43,6 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         types: action.payload,
-      };
-
-    case CLEAR_DETAIL:
-      return {
-        ...state,
-        selectedPokemon: {},
       };
 
     default:
