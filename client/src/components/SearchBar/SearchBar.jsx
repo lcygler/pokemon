3e858a7f6acca620869d9c23f10aa3e8da4ce0de
lcygler <leandrocygler@gmail.com>
2023-04-21 21:11:49 -0300

    update searchbar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { uuidRegex } from "../../utils/consts";
 import styles from "./SearchBar.module.css";
 
 function SearchBar() {
+  const history = useHistory();
+  const [idOrName, setIdOrName] = useState("");
+
+  const handleChange = (e) => {
+    setIdOrName(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      searchPokemon(idOrName);
+      setIdOrName("");
+    }
+  };
+
+  const searchPokemon = (idOrName) => {
+    if (!uuidRegex.test(idOrName) && isNaN(idOrName) && typeof idOrName !== "string") {
+      return window.alert("Invalid name or ID");
+    }
+
+    history.push(`/home/${idOrName}`);
+  };
+
   return (
     <div className={styles.container}>
-      <form>
-        <input className={styles.input} placeholder="Search for a pokemon..." />
-        <button className={styles.button}>Search</button>
-      </form>
+      <input
+        type="search"
+        value={idOrName}
+        className={styles.input}
+        placeholder="Search for a pokemon..."
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      />
+      <button
+        className={styles.searchButton}
+        onClick={() => {
+          searchPokemon(idOrName);
+          setIdOrName("");
+        }}
+      >
+        Search
+      </button>
       <Link to="/create" className={styles.link}>
         <span>Create</span>
       </Link>
