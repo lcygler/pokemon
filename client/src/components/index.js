4b39449c11d8a61302ecbaeab0afab0da1eb595c
lcygler <leandrocygler@gmail.com>
2023-04-23 15:34:49 -0300

    add FilterAndOrder component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Card from "./Card/Card";
 import Cards from "./Cards/Cards";
+import FilterAndOrder from "./FilterAndOrder/FilterAndOrder";
 import SearchBar from "./SearchBar/SearchBar";
 
-export { Card, Cards, SearchBar };
+export { Card, Cards, SearchBar, FilterAndOrder };
