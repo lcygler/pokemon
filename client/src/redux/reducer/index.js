59a4d96c02ac34c1f8a3e76cbe513c119854e874
lcygler <leandrocygler@gmail.com>
2023-04-24 00:02:06 -0300

    add filter and order actions

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,25 +1,28 @@
-import { CLEAR_DETAIL, CREATE_POKEMON, GET_ALL, GET_BY_ID, GET_BY_NAME, GET_TYPES } from "../actions";
+import {
+  CLEAR_DETAIL,
+  CREATE_POKEMON,
+  FILTER_BY_ORIGIN,
+  FILTER_BY_TYPE,
+  GET_ALL,
+  GET_BY_ID,
+  GET_BY_NAME,
+  GET_TYPES,
+  ORDER_BY_ATTACK,
+  ORDER_BY_NAME,
+} from "../actions";
 
 const initialState = {
   allPokemons: [],
   selectedPokemon: {},
   types: [],
+  typeFilter: "All",
+  originFilter: "All",
+  idOrder: "Default",
+  attackOrder: "Default",
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case CLEAR_DETAIL:
-      return {
-        ...state,
-        selectedPokemon: {},
-      };
-
-    case CREATE_POKEMON:
-      return {
-        ...state,
-        selectedPokemon: action.payload,
-      };
-
     case GET_ALL:
       return {
         ...state,
@@ -42,6 +45,43 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         types: action.payload,
+      };
+
+    case CREATE_POKEMON:
+      return {
+        ...state,
+        selectedPokemon: action.payload,
+      };
+
+    case CLEAR_DETAIL:
+      return {
+        ...state,
+        selectedPokemon: {},
+      };
+
+    //* Filter and Order
+    case FILTER_BY_ORIGIN:
+      return {
+        ...state,
+        //
+      };
+
+    case FILTER_BY_TYPE:
+      return {
+        ...state,
+        //
+      };
+
+    case ORDER_BY_ATTACK:
+      return {
+        ...state,
+        //
+      };
+
+    case ORDER_BY_NAME:
+      return {
+        ...state,
+        //
       };
 
     default:
