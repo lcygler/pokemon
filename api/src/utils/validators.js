0344db93e352604744437a9eb7b82133cbcdd3d5
lcygler <leandrocygler@gmail.com>
2023-04-25 21:25:28 -0300

    update validations

--- api/src/utils/validators.js
+++ api/src/utils/validators.js
@@ -1,6 +1,9 @@
-const validatePokemon = (newPokemon) => {
-  const { name, hp, attack, defense, speed, height, weight, image, types } = newPokemon;
-  const filteredTypes = types.filter(Boolean);
+const { urlRegex } = require("./consts");
+const uuid = require("uuid");
+
+const createValidate = (pokemon) => {
+  const { name, hp, attack, defense, speed, height, weight, image, types } = pokemon;
+  const filteredTypes = types?.filter(Boolean);
 
   //* Required fields
   if (![name, hp, attack, defense, image, types].every(Boolean)) {
@@ -23,12 +26,63 @@ const validatePokemon = (newPokemon) => {
     !Number.isInteger(hp) ||
     !Number.isInteger(attack) ||
     !Number.isInteger(defense) ||
-    !Number.isInteger(speed) ||
-    !Number.isInteger(height) ||
-    !Number.isInteger(weight)
+    (speed && !Number.isInteger(speed)) ||
+    (height && !Number.isInteger(height)) ||
+    (weight && !Number.isInteger(weight))
   ) {
     throw new Error("HP, attack, defense, speed, height and weight must be integers");
   }
+
+  //* Image
+  if (!urlRegex.test(image)) {
+    throw new Error("Image must be a valid URL");
+  }
 };
 
-module.exports = validatePokemon;
+const patchValidate = (idPokemon, pokemon) => {
+  const { name, hp, attack, defense, speed, height, weight, image, types } = pokemon;
+  const filteredTypes = types?.filter(Boolean);
+  const isUuid = uuid.validate(idPokemon);
+
+  //* Check for non-empty fields
+  if (![name, hp, attack, defense, speed, height, weight, image, types].some(Boolean)) {
+    throw new Error("At least one field is required");
+  }
+
+  //* DataType String
+  if ((name && typeof name !== "string") || (image && typeof image !== "string")) {
+    throw new Error("Name and image must be strings");
+  }
+
+  if (types) {
+    for (element of filteredTypes) {
+      if (typeof element !== "string") {
+        throw new Error("Types must be strings");
+      }
+    }
+  }
+
+  //* DataType Integer
+  if (
+    (hp && !Number.isInteger(hp)) ||
+    (attack && !Number.isInteger(attack)) ||
+    (defense && !Number.isInteger(defense)) ||
+    (speed && !Number.isInteger(speed)) ||
+    (height && !Number.isInteger(height)) ||
+    (weight && !Number.isInteger(weight))
+  ) {
+    throw new Error("HP, attack, defense, speed, height and weight must be integers");
+  }
+
+  //* UUID
+  if (!isUuid) {
+    throw new Error("Invalid ID");
+  }
+
+  //* Image
+  if (image && !urlRegex.test(image)) {
+    throw new Error("Image must be a valid URL");
+  }
+};
+
+module.exports = { createValidate, patchValidate };
