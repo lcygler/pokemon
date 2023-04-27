31e3c7263875d64593bec5109aacc8d44d0f2cbc
lcygler <leandrocygler@gmail.com>
2023-04-26 22:46:43 -0300

    remove api url

--- api/src/utils/consts.js
+++ api/src/utils/consts.js
@@ -1,12 +1,9 @@
-const API_URL = "https://pokeapi.co/api/v2";
-
 const imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
 
 const urlRegex =
   /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 
 module.exports = {
-  API_URL,
   imageRegex,
   urlRegex,
 };
