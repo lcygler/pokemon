5d00dad54e908df66edad14f7e937e9d33d4b504
lcygler <leandrocygler@gmail.com>
2023-04-27 04:34:30 -0300

    update cors

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,15 @@ const server = express();
 
 server.name = "API";
 
-server.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
+server.use(
+  cors({
+    origin: allowedOrigins,
+    // credentials: true
+  })
+);
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
@@ -23,7 +31,7 @@ server.use(morgan("dev"));
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
 //   res.header('Access-Control-Allow-Credentials', 'true');
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 //   next();
 // });
 
