a09540c781fede3d4b835f9be72bfb51a4f986c0
lcygler <leandrocygler@gmail.com>
2023-04-28 19:21:46 -0300

    update cors

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const cors = require("cors");
 const routes = require("./routes/index.js");
 
 require("./db.js");
@@ -18,22 +17,15 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-//   next();
-// });
-
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 
-server.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  next();
+});
 
 server.use("/", routes);
 
