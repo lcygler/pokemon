dc9c3f6eb8d2167ae181ea2efe5c2f39d60bc970
lcygler <leandrocygler@gmail.com>
2023-04-28 01:23:08 -0300

    set pg as dialect module

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const pg = require("pg");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, NODE_ENV } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   ssl: NODE_ENV === "production", // enable SSL only in production mode
+  dialectModule: pg, // Set the pg module as the dialectModule
   dialectOptions: {
     ssl: NODE_ENV === "production", // enable SSL only in production mode
   },
