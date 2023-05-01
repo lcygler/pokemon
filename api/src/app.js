bfe7f43eee1a0d7b43c31c49b008090d93a5be96
lcygler <leandrocygler@gmail.com>
2023-04-30 23:54:48 -0300

    update cors config

--- api/src/app.js
+++ api/src/app.js
@@ -17,10 +17,13 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",") || "http://localhost:3000";
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || "http://localhost:3000").split(",");
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins); // update to match the domain you will make the request from
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
