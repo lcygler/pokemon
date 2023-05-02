a0195ebe8ddf21bd77343b4ebaee0186a2146ab6
lcygler <leandrocygler@gmail.com>
2023-05-01 22:38:17 -0300

    add require dotenv

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
